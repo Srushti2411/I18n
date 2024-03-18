17b5e10142ca78dc46dd415ed3c5a42ac749717b
Srushti2411 <srushtigarad24@gmail.com>
2024-03-18 12:51:01 +0530

    Applied Internationalisation

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Home.css"
+import I18n from  '../../Utils/I18n'
 
 function Home() {
+
+  const [lang, setLang] = useState("en");
+  
   return (
-    <div>Home</div>
+   <div>
+    <h1>{I18n( "WelcomeMessage")}</h1>
+
+    <select 
+     defaultValue={localStorage.getItem("lang")}
+     onChange={(e)=>{
+      localStorage.setItem("lang",e.target.value);
+      window.location.reload();
+    }}>
+      <option value="mr">Marathi</option>
+      <option value="hi">Hindi</option>
+      <option value="en">English</option>
+    </select>
+
+  
+   </div>
   )
 }
 
